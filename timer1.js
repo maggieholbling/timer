f1e54b807e920c28cf98fc335e32229df3b90061
Maggie <maggieholbl@gmail.com>
2019-08-27 22:21:52 +0000

    initial commit with @reza477 - pair programming

---
+++ timer1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const alarm = function (arr, callback) {
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] >= 0 && !isNaN(Number(arr[i]))) {
+      setTimeout(() => {
+        callback();
+      }, Number(arr[i]) * 1000)
+    }
+  }
+
+ 
+}
+
+
+alarm(process.argv.slice(2), () => {
+  process.stdout.write('\x07');
+});
